f0d5f8ed06881751fde8c8c55585e7a45a431396
sohalt <sohalt@sohalt.net>
2024-05-14 16:59:06 -0400

    ferris-wheel

--- src/rollercoaster.clj
+++ src/rollercoaster.clj
@@ -39,6 +39,57 @@
                         :offset-y (/ h 4)
                         :period 40}))))
 
+(defn path
+  ([points] (path points {}))
+  ([points {:keys [close?] :or {close? true}}]
+   (let [start-points points
+         end-points (cond-> (vec (rest points))
+                      close? (conj (first points)))]
+     (map (fn [start end] [start end])
+          start-points end-points))))
+
+(defn gondola [[top-x top-y]]
+  (let [roof-height 5
+        railing-height 8
+        width 20
+        height 20
+        left (- top-x (/ width 2))
+        right (+ top-x (/ width 2))
+        roof-bottom (+ top-y roof-height)
+        bottom (+ top-y height)
+        railing (- bottom railing-height)]
+    (concat (path [[top-x top-y]
+                   [left roof-bottom]
+                   [left bottom]
+                   [right bottom]
+                   [right roof-bottom]])
+            [[[left roof-bottom] [right roof-bottom]]
+             [[left railing] [right railing]]])))
+
+(def tau (* 2 Math/PI))
+
+(defn point-on-circle [[cx cy] r angle]
+  [(+ cx (* r (Math/sin angle))) (+ cy (* r (Math/cos angle)))])
+
+(def num-segments 20)
+(defn circle [[cx cy] r]
+  (->> (range 0 tau (/ tau num-segments))
+       (map (fn [angle] (point-on-circle [cx cy] r angle)))
+       (path)))
+
+(defn ferris-wheel
+  ([[cx cy] r] (ferris-wheel [cx cy] r 0))
+  ([[cx cy] r t]
+   (let [num-gondolas 10
+         points-on-circle (map (fn [angle]
+                                 (point-on-circle [cx cy] r (+ t angle)))
+                               (range 0 tau (/ tau num-gondolas)))
+         spokes (map (fn [p] [[cx cy] p]) points-on-circle)
+         gondolas (mapcat gondola points-on-circle)]
+     (concat (circle [cx cy] r)
+             spokes
+             gondolas))))
+
 (defn vertical-line
   ([x]
    (vertical-line x h))
@@ -69,10 +120,14 @@
             (mapcat trace-segments descending-f)
             (mapcat trace-segments ascending-f))))
 
+(defn roller-coaster []
+  (concat (trace-segments track)
+          (scaffolding {})))
+
 (defn draw []
   (q/background 255)
-  (doall (map (partial apply q/line) (trace-segments track)))
-  (doall (map (partial apply q/line) (scaffolding {}))))
+  (doall (map (partial apply q/line) (roller-coaster)))
+  (doall (map (partial apply q/line) (ferris-wheel [(/ w 1.2) (/ h 2.5)] 100 (/ (q/frame-count) 100)))))
 
 (q/defsketch rollercoaster
   :size [w h]
